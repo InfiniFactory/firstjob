a51391426aca7c6feb869515f865536cea06a923
InfiniFactory <102616556+InfiniFactory@users.noreply.github.com>
2021-08-19 09:37:15 +0000

    cifs: on CONFIG_HIGHMEM machines, limit the rsize/wsize to the kmap space

--- lib/teuxdeux/client/connection.rb
+++ lib/teuxdeux/client/connection.rb
@@ -25,4 +25,4 @@ module TeuxDeux
           end
           builder.adapter(adapter)
         end
-        connection.basic_auth authentication[:login], auth
+        connection.basic_auth authentication[:login], authenticat
