1933fef7cdcc0b3e6accc916c549968673f4b870
InfiniFactory <102616556+InfiniFactory@users.noreply.github.com>
2021-08-20 08:59:37 +0000

    fix silly typo

--- lib/teuxdeux/client/connection.rb
+++ lib/teuxdeux/client/connection.rb
@@ -25,4 +25,4 @@ module TeuxDeux
           end
           builder.adapter(adapter)
         end
-        connection.basic_auth authentication[:login], authenticat
+        connection.basic_auth authentication[:login], authentication[:password] 
