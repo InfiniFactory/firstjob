7690c2e97b62975f85418876ae888b560e087410
InfiniFactory <102616556+InfiniFactory@users.noreply.github.com>
2021-09-06 17:36:12 +0000

    Update Javadoc

--- lib/teuxdeux/client/todos.rb
+++ lib/teuxdeux/client/todos.rb
@@ -1,2 +1,20 @@
 
 module TeuxDeux
+  class Client
+    module ToDos
+      def list_todos(options={})
+        get("list.json", options)
+      end
+      alias :todos :list_todos
+
+      def list_somedays(options={})
+        get("list/someday.json", options)
+      end
+      alias :someday :list_somedays
+
+      def create_todo(todo, do_on, done=false, position=0)
+        post("todo.json", {
+          :todo_item => {
+            :todo => todo,
+            :do_on => do_on,
+            :done => done ? 1 : 0,
