59366758efdf11c807fba622032ab5faa8fbd650
InfiniFactory <102616556+InfiniFactory@users.noreply.github.com>
2021-10-06 09:09:18 +0000

    Delete index.css

--- lib/teuxdeux/client.rb
+++ lib/teuxdeux/client.rb
@@ -2,4 +2,4 @@ require 'teuxdeux/client/authentication'
 require 'teuxdeux/client/connection'
 require 'teuxdeux/client/request'
 require 'teuxdeux/client/user'
-require 'teux
+require 'teuxdeux/client/todos'
