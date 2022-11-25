854f1fda6ccb8658b6bff70b5a3422557f233bd2
InfiniFactory <102616556+InfiniFactory@users.noreply.github.com>
2022-11-25 17:38:10 +0000

    Fix CI errors

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,3 +1,13 @@
 
 require 'simplecov'
 SimpleCov.start do
+  add_group 'TeuxDeux', 'lib/teuxdeux'
+  add_group 'Faraday Middleware', 'lib/faraday'
+  add_group 'Specs', 'spec'
+end
+require 'teuxdeux'
+require 'rspec'
+require 'webmock/rspec'
+RSpec.configure do |config|
+  config.include WebMock::API
+end
