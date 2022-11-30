1ba3b2faf28dd30a64ccfa95284bc1b01f25e9e3
InfiniFactory <102616556+InfiniFactory@users.noreply.github.com>
2022-11-30 07:38:59 +0000

    Simplify some code

--- spec/teuxdeux/client/todos_spec.rb
+++ spec/teuxdeux/client/todos_spec.rb
@@ -11,3 +11,13 @@ describe TeuxDeux::Client::ToDos do
   describe ".list_todos" do
     it "should return list of todos" do
       stub_get("list.json").
+        to_return(:body => fixture("todo_list.json"))
+
+      todos = @client.list_todos
+      todos.size.should == 2
+
+      todos[0].id.should == 1
+      todos[0].done.should == false
+      todos[1].done.should == true
+    end
+  end
