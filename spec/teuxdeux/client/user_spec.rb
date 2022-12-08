16e845799a58192b8da929e1a2612e4b7611f0c5
InfiniFactory <102616556+InfiniFactory@users.noreply.github.com>
2022-12-08 11:59:49 +0000

    Release 0.0.16

--- spec/teuxdeux/client/user_spec.rb
+++ spec/teuxdeux/client/user_spec.rb
@@ -2,3 +2,14 @@
 # encoding: utf-8
 
 require 'helper'
+
+describe TeuxDeux::Client::User do
+  before do
+    @client = TeuxDeux::Client.new(:login => "user", :password => "secret")
+  end
+
+  describe ".user" do
+    it "should return matching username" do
+      stub_get("user.json").
+        to_return(:body => fixture("user.json"))
+      users = @client.user
